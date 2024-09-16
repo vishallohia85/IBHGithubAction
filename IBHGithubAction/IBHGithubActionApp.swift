092c231cca6edb2dcfa1fb58f29a5581f9909128
Vishal Lohia <vishal.lohia@programming.com>
2024-09-16 15:19:26 +0530

    Initial Commit

---
+++ IBHGithubAction/IBHGithubActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBHGithubActionApp.swift
+//  IBHGithubAction
+//
+//  Created by Mobileprogramming on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct IBHGithubActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
